24dabd55e117d8ee46b627462f298674fba883a4
Baus-san <edzonbausa33@gmail.com>
2022-05-14 12:12:46 +0800

    Fixed some typo

--- components/UI/EmptyPlaceholder.tsx
+++ components/UI/EmptyPlaceholder.tsx
@@ -6,7 +6,7 @@ const EmptyPlaceholder = () => {
     <div className="grid h-4/5 place-items-center ">
       <Image src={imgPlaceholder} width={720} height={720} />
       <div className="text-2xl text-slate-700 font-medium -mt-48">
-        This Module is currently under Production
+        This Module is currently under development
       </div>
     </div>
   );
